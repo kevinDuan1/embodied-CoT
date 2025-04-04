6cea6738554c25e3265d98bf989bc4a9e85456b5
Yuan Zhang <mike.zy7@gmail.com>
2025-04-04 12:58:10 +0000

    [fix] visual between gripper and action

--- experiments/robot/libero/libero_utils.py
+++ experiments/robot/libero/libero_utils.py
@@ -179,12 +179,12 @@ def split_reasoning(text, tags):
 class CotTag(enum.Enum):
     TASK = "TASK:"
     PLAN = "PLAN:"
-    VISIBLE_OBJECTS = "VISIBLE OBJECTS:"
     SUBTASK_REASONING = "SUBTASK REASONING:"
     SUBTASK = "SUBTASK:"
     MOVE_REASONING = "MOVE REASONING:"
     MOVE = "MOVE:"
     GRIPPER_POSITION = "GRIPPER POSITION:"
+    VISIBLE_OBJECTS = "VISIBLE OBJECTS:"
     ACTION = "ACTION:"
 
 
@@ -192,12 +192,12 @@ def get_cot_tags_list():
     return [
         CotTag.TASK.value,
         CotTag.PLAN.value,
-        CotTag.VISIBLE_OBJECTS.value,
         CotTag.SUBTASK_REASONING.value,
         CotTag.SUBTASK.value,
         CotTag.MOVE_REASONING.value,
         CotTag.MOVE.value,
         CotTag.GRIPPER_POSITION.value,
+        CotTag.VISIBLE_OBJECTS.value,
         CotTag.ACTION.value,
     ]
 
--- prismatic/util/cot_utils.py
+++ prismatic/util/cot_utils.py
@@ -4,12 +4,12 @@ import enum
 class CotTag(enum.Enum):
     TASK = "TASK:"
     PLAN = "PLAN:"
-    VISIBLE_OBJECTS = "VISIBLE OBJECTS:"
     SUBTASK_REASONING = "SUBTASK REASONING:"
     SUBTASK = "SUBTASK:"
     MOVE_REASONING = "MOVE REASONING:"
     MOVE = "MOVE:"
     GRIPPER_POSITION = "GRIPPER POSITION:"
+    VISIBLE_OBJECTS = "VISIBLE OBJECTS:"
     ACTION = "ACTION:"
 
 
@@ -21,12 +21,12 @@ def get_cot_tags_list():
     return [
         CotTag.TASK.value,
         CotTag.PLAN.value,
-        CotTag.VISIBLE_OBJECTS.value,
         CotTag.SUBTASK_REASONING.value,
         CotTag.SUBTASK.value,
         CotTag.MOVE_REASONING.value,
         CotTag.MOVE.value,
         CotTag.GRIPPER_POSITION.value,
+        CotTag.VISIBLE_OBJECTS.value,
         CotTag.ACTION.value,
     ]
 
@@ -35,11 +35,11 @@ def get_cot_database_keys():
     return {
         CotTag.TASK.value: "task",
         CotTag.PLAN.value: "plan",
-        CotTag.VISIBLE_OBJECTS.value: "bboxes",
         CotTag.SUBTASK_REASONING.value: "subtask_reason",
         CotTag.SUBTASK.value: "subtask",
         CotTag.MOVE_REASONING.value: "move_reason",
         CotTag.MOVE.value: "move",
         CotTag.GRIPPER_POSITION.value: "gripper",
+        CotTag.VISIBLE_OBJECTS.value: "bboxes",
         CotTag.ACTION.value: "action",
     }
