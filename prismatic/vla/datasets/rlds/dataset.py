84539c08d44be88c9f75ea3ebe8d820be9931937
kevinDuan01 <zhekaiduan2312@gmail.com>
2025-04-15 11:08:14 +0100

    fix missing reasoning v1

--- prismatic/vla/datasets/rlds/dataset.py
+++ prismatic/vla/datasets/rlds/dataset.py
@@ -296,7 +296,7 @@ def make_dataset_from_rlds(
                 episode_ids[first_empty],
                 indices[first_empty]
             ], separator="_")
-            # tf.print("Empty reasoning for trajectory", detail)
+            tf.print("Empty reasoning for trajectory", detail)
 
         traj = {
             "observation": new_obs,
@@ -696,7 +696,7 @@ def make_interleaved_dataset(
 
     # Shuffle the Dataset
     #   =>> IMPORTANT :: Shuffle AFTER .cache(), or else memory will still leak!
-    dataset = dataset.shuffle(shuffle_buffer_size)
+    # dataset = dataset.shuffle(shuffle_buffer_size)
 
     # Apply Frame Transforms
     overwatch.info("Applying frame transforms on dataset...")
