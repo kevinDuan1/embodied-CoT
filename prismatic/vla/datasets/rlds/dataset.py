29e6e85d3b18287034ac2a2d2cf79f48eed64711
kevinDuan01 <zhekaiduan2312@gmail.com>
2025-04-15 11:37:44 +0100

    fix reasoning miss

--- prismatic/vla/datasets/rlds/dataset.py
+++ prismatic/vla/datasets/rlds/dataset.py
@@ -696,7 +696,7 @@ def make_interleaved_dataset(
 
     # Shuffle the Dataset
     #   =>> IMPORTANT :: Shuffle AFTER .cache(), or else memory will still leak!
-    dataset = dataset.shuffle(shuffle_buffer_size)
+    # dataset = dataset.shuffle(shuffle_buffer_size)
 
     # Apply Frame Transforms
     overwatch.info("Applying frame transforms on dataset...")
